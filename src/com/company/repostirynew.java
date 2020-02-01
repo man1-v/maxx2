5f5277df56d42c9f7d505156ad3b17b94b1e387c
mansurx2009 <itcom.0219@gmail.com>
2020-02-01 12:13:55 -0500

    100

--- src/com/company/repostirynew.java
+++ src/com/company/repostirynew.java
@@ -5,5 +5,12 @@ public class repostirynew {
 
         byte a = 125;
         System.out.println(a);
+        boolean b = true;
+        float c = 1.5f;
+        int d = 10;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
     }
 }
